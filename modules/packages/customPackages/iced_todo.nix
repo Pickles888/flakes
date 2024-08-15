733de38139b9acdae09a39ad3cc76f6900604df9
Pickles888 <verysaltypickles@gmail.com>
2024-08-15 16:35:38 -0700

    update latest version for iced-todo

--- modules/packages/customPackages/iced_todo.nix
+++ modules/packages/customPackages/iced_todo.nix
@@ -6,7 +6,7 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "iced-todo";
-  version = "1.0.0";
+  version = "1.0.1";
 
   src = fetchFromGitHub {
     owner = "Pickles888";
@@ -42,7 +42,7 @@ rustPlatform.buildRustPackage rec {
   ];
 
   postFixup = ''
-    wrapProgram $out/bin/iced-todo \
+    wrapProgram $out/bin/${pname} \
       --suffix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath buildInputs} \
       --suffix WINIT_UNIX_BACKEND : "wayland"
   '';
